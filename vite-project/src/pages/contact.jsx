ba0da11817031b70b591512303826e36a96a82ab
Mackenzie Jessup <Mackenzie.jessup@mykenzie.snhu.edu>
2023-11-22 12:21:36 -0500

    form can't submit when empty + thanks for email

--- vite-project/src/pages/contact.jsx
+++ vite-project/src/pages/contact.jsx
@@ -11,10 +11,23 @@ function Contact () {
         reply_to: '',
       })
 
+    const [submit, hasSubmit] = useState(false)
+
+    function Submitted () {
+        if (submit === true) {
+            return <p>Thank you! I will respond as soon as I can.</p>
+        } else if (toSend.from_name === '' || toSend.message === '' || toSend.reply_to === ''){
+            return <p>Please fill out all inputs.</p>
+        } else {
+            return
+        }
+    }
+
     const handleSubmit = (e) => {
         e.preventDefault()
         console.log(toSend)
-        emailjs.send(
+        if (toSend.from_name !== '' && toSend.message !== '' && toSend.reply_to !== ''){
+            emailjs.send(
             'service_8sjcepz',
             'template_vmp8nlb',
             toSend,
@@ -22,44 +35,50 @@ function Contact () {
           )
             .then((response) => {
                 console.log('SUCCESS!', response.status, response.text);
+                hasSubmit(true)
             })
             .catch((err) => {
                 console.log('FAILED...', err);
-            });
+                hasSubmit(false)
+            })
+        } else {
+            console.log('FAILED...');
+            hasSubmit(false)
+        }
     }
 
-
-return (
-    <>
-        <h1 className="title">Contacts</h1>
-        <h4 className="link">
-            LinkedIn: <a style={{textDecoration:"solid"}} target='_blank' rel='noopener noreferrer' href="https://www.linkedin.com/in/mackenzie-jessup-527198278">Mackenzie Jessup</a>
-        </h4>
-        <div className='form'>
-            <h3>Email Me</h3>
-            <form onSubmit={handleSubmit}>
-                <div className="form-group">
-                    <label htmlFor="name">Name</label>
-                    <input type="text" className="form-control" value={toSend.from_name} onChange={(e) => {
-                        setToSend({...toSend, from_name: e.target.value}) 
-                    }}/>
-                </div>
-                <div className="form-group">
-                    <label htmlFor="exampleInputEmail1">Email address</label>
-                    <input type="email" className="form-control" aria-describedby="emailHelp" value={toSend.reply_to} onChange={(e) => {
-                        setToSend({...toSend, reply_to: e.target.value}) 
-                    }}/>
-                </div>
-                <div className="form-group">
-                    <label htmlFor="message">Message</label>
-                    <textarea className="form-control" rows="5" value={toSend.message} onChange={(e) => {
-                        setToSend({...toSend, message: e.target.value}) 
-                    }}></textarea>
-                </div>
-                <button type="submit" className="btn btn-primary">Send</button>
-            </form>
-        </div>
-    </>
-)
+    return (
+        <>
+            <h1 className="title">Contacts</h1>
+            <h4 className="link">
+                LinkedIn: <a style={{textDecoration:"solid"}} target='_blank' rel='noopener noreferrer' href="https://www.linkedin.com/in/mackenzie-jessup-527198278">Mackenzie Jessup</a>
+            </h4>
+            <div className='form'>
+                <h3>Email Me</h3>
+                <form onSubmit={handleSubmit}>
+                    <div className="form-group">
+                        <label htmlFor="name">Name</label>
+                        <input type="text" id="name" className="form-control" value={toSend.from_name} autoComplete="given-name" onChange={(e) => {
+                            setToSend({...toSend, from_name: e.target.value}) 
+                        }}/>
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="email">Email address</label>
+                        <input type="email" id='email' autoComplete="email" className="form-control" aria-describedby="emailHelp" value={toSend.reply_to} onChange={(e) => {
+                            setToSend({...toSend, reply_to: e.target.value}) 
+                        }}/>
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="message">Message</label>
+                        <textarea id='message' className="form-control" rows="5" value={toSend.message} onChange={(e) => {
+                            setToSend({...toSend, message: e.target.value}) 
+                        }}></textarea>
+                    </div>
+                    <button type="submit" disabled={submit} className="btn btn-primary">Send</button>
+                    <Submitted />
+                </form>
+            </div>
+        </>
+    )
 }
 export default Contact
