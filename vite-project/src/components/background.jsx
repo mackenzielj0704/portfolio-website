3b95be39bcd0ad9aceedbc84fa51101001db1a68
Mackenzie Jessup <Mackenzie.jessup@mykenzie.snhu.edu>
2023-11-26 22:30:58 -0500

    changed background color+ fixed link length

--- vite-project/src/components/background.jsx
+++ vite-project/src/components/background.jsx
@@ -86,23 +86,15 @@ function Background () {
 
     for (let i = 1; i < 3201; i++) {
         let div = document.createElement("div")
-        let random = Math.round(Math.random() * 10)
-        if (i % random === 0){
             div.style.height = "2.5%"
             div.style.width = "1.25%"
-            div.style.backgroundColor = "blue"
-        }
-        else {
-            div.style.height = "2.5%"
-            div.style.width = "1.25%"
-            div.style.backgroundColor = "green"
-        }
+            div.style.backgroundColor = "rgb(96, 120, 234)"
         background.append(div)
         div.addEventListener("mouseover",() => {
-            if (div.style.backgroundColor === "blue") {
-                div.style.backgroundColor = "green"
-            } else {
-                div.style.backgroundColor = "blue"
+            if (div.style.backgroundColor === "rgb(23, 234, 217)") {
+                div.style.backgroundColor = "rgb(96, 120, 234)"
+            } else{
+                div.style.backgroundColor = "rgb(23, 234, 217)"
             }
         })
     }
