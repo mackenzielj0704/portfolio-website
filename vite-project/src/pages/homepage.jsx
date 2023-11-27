dc9069e84d81ceff928cc211d775efb599eacd20
Mackenzie Jessup <Mackenzie.jessup@mykenzie.snhu.edu>
2023-11-27 11:46:47 -0500

    changed homepage info + css

--- vite-project/src/pages/homepage.jsx
+++ vite-project/src/pages/homepage.jsx
@@ -4,18 +4,11 @@ function Homepage () {
 
     return (
         <>
-        <h1 className="title">Mackenzie Jessup</h1>
-        <div className="littletext">
-                <h5>MERN Developer</h5>
-            </div>
+        <h1 className="title">Hi, my name is <br/>Mackenzie Jessup</h1>
+        <hr></hr>
+        <h5 className="littletext">Web Developer</h5>
         <div className="boxes">
-            <div className="careerGoals">
-                <h2>Welcome!</h2>
-                <p>As a recent graduate I am excited to start working as a Developer.</p>
-                <p></p>
-            </div>
             <div className="skills">
-                <h2>Skills</h2>
                 <p>MongoDB, Express.js, Node.js, React.js, HTML/CSS, JavaScript</p>
             </div>
         </div>
