c618df931047055bab171383dcdd70b60cb66012
Mackenzie Jessup <Mackenzie.jessup@mykenzie.snhu.edu>
2023-12-01 16:21:51 -0500

    changed videos to youtube links, instead of files

--- vite-project/src/pages/projects.jsx
+++ vite-project/src/pages/projects.jsx
@@ -1,8 +1,5 @@
 
 import './pageStyles/projects.css'
-import MazeRecoring from "../components/mazeRecording.mp4"
-import ShelfHelpRecording from "../components/Shelf-helpRecording.mp4"
-import PigLatinRecording from "../components/PigLatinRecording.mp4"
 
 function Projects () {
 
@@ -13,10 +10,7 @@ function Projects () {
             <div className="project" id='one'>
                 <h2 className='h2'>Maze/Sokoban Game</h2>
                 <div>
-                <video width="50%" height="50%" controls className='video'>
-                    <source src={MazeRecoring} type="video/mp4" />
-                    Your browser does not support the video tag.
-                </video>
+                <iframe width="50%" height="50%" src="https://youtube.com/embed/7fcBXATiO2k" id='mazeV'/>
                     <p>
                         A one-person project that was made with HTML, JS, and CSS.
                         <br /> This site contains two games, a maze with three maps and sokoban with two.
@@ -25,10 +19,7 @@ function Projects () {
             </div>
             <div className="project" id='two'>
                 <h2 className='h2'>Shelf-Help</h2>
-                <video width="70%" height="70%" controls className='video'>
-                    <source src={ShelfHelpRecording} type="video/mp4" />
-                    Your browser does not support the video tag.
-                </video>
+                <iframe width="70%" height="70%" src="https://youtube.com/embed/IqP3iQPvBsE" id='shelfV'/>
                 <div>
                     <h4 className='h2'><a target='_blank' rel='noopener noreferrer' href="http://54.81.116.198/" style={{textDecoration:"solid"}}>Capstone Project</a></h4>
                     <p>
@@ -39,10 +30,7 @@ function Projects () {
             <div className="project" id='three'>
                 <h2 className='h2'>Pig Latin Translator</h2>
                 <div id='pigLatin'>
-                <video width="47%" height="47%" controls className='video'>
-                    <source src={PigLatinRecording} type="video/mp4" />
-                    Your browser does not support the video tag.
-                </video>
+                <iframe width="47%" height="47%" src="https://youtube.com/embed/64hu596Barc" id='pigV'/>
                     <p className='pigLatinText' style={{marginLeft: "3%"}}>
                         A one-person project made with HTML, CSS, and JS. 
                         <br /> This site translates english to Pig-latin and Pig-latin to english live.
